944e053b05e5d3cc004b04282776e3398299160b
Ryan S <ryan.schneiderman@tufts.edu>
2018-08-24 13:17:33 -0400

    react tutorial

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
+
+class App extends Component{
+	state = {
+			characters: []
+		};
+	removeCharacter = index => {
+		const {characters} = this.state;
+
+		this.setState({
+			characters: characters.filter((characters, i) =>{
+				return i !== index;
+			})
+		});
+	}
+	handleSubmit = character => {
+		this.setState({
+			characters: [...this.state.characters, character]
+		})
+	}		
+	render() {
+		
+		return (
+			<div className="Container">
+				<Table 
+				characterData = {this.state.characters}
+				removeCharacter={this.removeCharacter}
+				/>
+				<Form handleSubmit={this.handleSubmit}/>	
+			</div>
+		);
+	}
+	
+}
+
+export default App;
